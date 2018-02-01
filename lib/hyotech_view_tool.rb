801b4a45c251d43276a85b97943af889012b7cb4
Michael Orr <michaelorr@Michaels-MacBook-Pro.local>
2018-02-01 09:38:17 -0800

    apple

--- lib/hyotech_view_tool.rb
+++ lib/hyotech_view_tool.rb
@@ -1,2 +1,6 @@
 require "hyotech_view_tool/version"
 require "hyotech_view_tool/renderer"
+
+module HyotechViewTool
+  # Your code goes here...
+end
--- lib/hyotech_view_tool/renderer.rb
+++ lib/hyotech_view_tool/renderer.rb
@@ -5,3 +5,4 @@ module HyotechViewTool
     end
   end
 end
+
